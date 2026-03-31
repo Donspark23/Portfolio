5ddaa1a6a173d5308305061ae4dae080ce186fab
Uchenna Chidera Onyesom <onyuchennachidera@gmail.com>
2026-03-31 04:00:24 +0100

    fix: reset css clean

--- frontend/vite-project/src/main.jsx
+++ frontend/vite-project/src/main.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <h1 style={{ color: "black" }}>App Working ✅</h1>
 );
